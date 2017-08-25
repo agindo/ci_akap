94ac90a478489a383ccf0b120c12072521e75483
agindo <ramadhanagindo@gmail.com>
2017-08-25 12:56:50 +0700

    update

---
+++ application/views/footer.php
@@ -0,0 +1,17 @@
+		<footer class="footer">
+      <div class="container">
+        <p class="text-muted"><?php echo "&copy; ".date('Y');?></p>
+      </div>
+    </footer>
+
+
+    <script src="<?php echo base_url();?>assets/jquery/jquery-2.1.4.min.js"></script>
+	  <script src="<?php echo base_url();?>assets/bootstrap/js/bootstrap.min.js"></script>
+		<script src="<?php echo base_url();?>assets/datatables/js/jquery.dataTables.min.js"></script>
+		<script src="<?php echo base_url();?>assets/datatables/js/dataTables.bootstrap.js"></script>
+    <script src="<?php echo base_url();?>assets/datepicker/js/bootstrap-datepicker.min.js"></script>
+    <script src="<?php echo base_url();?>assets/clockpicker/js/bootstrap-clockpicker.js"></script>
+		<script type="text/javascript">
+		</script>
+  </body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+    <title>...</title> 
+    <link href="<?php echo base_url();?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/custom/sticky-footer-navbar.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/custom/portlets.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/custom/custom.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/datatables/css/dataTables.bootstrap.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/datepicker/css/bootstrap-datepicker3.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/clockpicker/css/bootstrap-clockpicker.css" rel="stylesheet">
+  </head>
+
+  <body style="background-color: #D5DED9">
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top" style="background-color:#FFF;border-color:#D5DED9">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#" style="color:#000000">BUS <i class="fa fa-bus"></i> AKAP</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse ">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-clipboard"></i>  Data Master <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-ticket"></i> Tiket <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-file-text-o"></i>  Laporan <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+            <li class="active"><a href="#"><kbd>LOGOUT <i class="fa fa-sign-out"></i></kbd></a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  
+   
